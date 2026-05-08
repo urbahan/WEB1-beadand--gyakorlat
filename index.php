6895531b3290abdf90628f5887524b93940e12f9
urbahan <jmnumy@hallgato.nje.hu>
2026-05-08 09:03:02 +0200

    index.php - VÉGLEGESITETT

--- index.php
+++ index.php
@@ -1,44 +1,23 @@
-<section class="cimlap-tartalom">
-    <h2>Üdvözöljük a Napfény Tours Utazási Irodánál!</h2>
-    <p>Fedezze fel velünk Tunézia és Egyiptom legszebb tájait. 2011-es tavaszi ajánlataink között mindenki megtalálja a számítását!</p>
+<?php
+session_start();
+require_once 'config/db.php'; 
 
-    <div class="video-container">
-        <h3>Kedvcsináló videóink</h3>
-        <div class="video-row" style="display: flex; gap: 20px; flex-wrap: wrap;">
-            
-            <!-- Saját videó: max 5 mp[cite: 1] -->
-            <div class="sajat-video">
-                <p>Irodánk pillanatai (Saját):</p>
-                <video width="320" height="240" controls autoplay muted loop>
-                    <source src="./videos/utazas.mp4" type="video/mp4">
-                    A böngésző nem támogatja a videót.
-                </video>
-            </div>
+$page = $_GET['page'] ?? 'home';
 
-            
-            <div class="youtube-video">
-                <p>Úticéljaink (YouTube):</p>
-                <iframe width="320" height="240" 
-                    src="https://www.youtube.com/embed/BFS9n4B_2xA?si=6XfPbb_HMR5KIwpr" 
-                    title="YouTube video player" frameborder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                    allowfullscreen>
-                </iframe>
-            </div>
-        </div>
-    </div>
 
-    <div class="terkep-container" style="margin-top: 30px;">
-        <h3>Irodánk elhelyezkedése</h3>
-        <p>Látogasson el hozzánk személyesen is!</p>
-        <!-- Google Térkép[cite: 1] -->
-        <div class="mapouter">
-            <div class="gmap_canvas">
-                <iframe width="100%" height="400" id="gmap_canvas" 
-                    src="https://maps.google.com/maps?q=Budapest,M%C3%BAzeum%20krt.%209&t=&z=15&ie=UTF8&iwloc=&output=embed" 
-                    frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
-                </iframe>
-            </div>
-        </div>
-    </div>
-</section>
+ob_start();
+
+
+global $pdo;
+
+switch ($page) {
+    case 'login':    require 'controllers/authcontroller.php'; break;
+    case 'contact':  require 'controllers/contactcontroller.php'; break;
+    case 'travel':   require 'controllers/travelcontroller.php'; break;
+    case 'images':   require 'controllers/imagecontroller.php'; break;
+    case 'messages': require 'controllers/messagecontroller.php'; break;
+    default:         require 'controllers/homecontroller.php'; break;
+}
+
+$content = ob_get_clean();
+include 'views/layout.php';
